d8d9229967c751cea205e1bd112ee0978aa5f7d5
rbenavente <63422736+rbenavente@users.noreply.github.com>
2021-12-02 16:15:46 +0100

    Update ec2.tf

--- terraform/simple_instance/ec2.tf
+++ terraform/simple_instance/ec2.tf
@@ -11,33 +11,14 @@ resource "aws_security_group" "ssh_traffic" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    git_commit           = "1f626f72c4e4055276e6ac5b2e984be2a9bdb25d"
-    git_file             = "terraform/simple_instance/ec2.tf"
-    git_last_modified_at = "2021-11-15 11:41:31"
-    git_last_modified_by = "63422736+rbenavente@users.noreply.github.com"
-    git_modifiers        = "63422736+rbenavente"
-    git_org              = "rbenavente"
-    git_repo             = "terragoat"
-    yor_trace            = "2e96ca80-c88f-4b7f-b2cc-c28bb11c913c"
-  }
+  
 }
 
 resource "aws_instance" "web_server_instance" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ssh_traffic.name}"]
-  tags = {
-    Name                 = "bc_workshop_ec2"
-    git_commit           = "1f626f72c4e4055276e6ac5b2e984be2a9bdb25d"
-    git_file             = "terraform/simple_instance/ec2.tf"
-    git_last_modified_at = "2021-11-15 11:41:31"
-    git_last_modified_by = "63422736+rbenavente@users.noreply.github.com"
-    git_modifiers        = "63422736+rbenavente"
-    git_org              = "rbenavente"
-    git_repo             = "terragoat"
-    yor_trace            = "390d6fbf-2da4-4728-9c97-44f8b58867f9"
-  }
+  
 }
 
 data "aws_ami" "ubuntu" {
